67636af0f77ad247498fb66e238ff3555b6de6c8
Craig Derington <craig@craigderington.me>
2017-01-09 11:58:13 -0500

    Update project root, remove test files.

--- tests.py
+++ tests.py
@@ -7,25 +7,24 @@ import json
 
 payload = {
     "network_id": "ORLFL01_7077",
-    "service_address": 1276,
+    "service_address": 1312,
     "receiver_time": "2017-01-04 13:16:17",
     "sensor_value": "38.39"
 }
 
 try:
     s = requests.Session()
-    s.auth = ('digitaldispatch', 'owlsitedemo')
-    s.headers.update({'x-test': 'true', 'X-CSRFToken': '7BY4ZQHQTXRuL4FrmuVQGb1GqtBi6EIR'})
+    s.auth = (config.loginusername, config.loginpassword)
+    s.headers.update({'x-test': 'true', 'X-CSRFToken': config.Token})
     r = s.get('http://localhost:5880/api-auth/login/',
               auth=HTTPBasicAuth(username=config.loginusername, password=config.loginpassword),
               headers=s.headers)
-    csrftoken = r.cookies['csrftoken']
 
     # Interact with the API.
-    tank_id = 1243
+    tank_id = config.tank_id
     response = requests.put('http://localhost:5880/api/tanks/' + str(tank_id) + '/',
                             auth=HTTPBasicAuth(username=config.loginusername, password=config.loginpassword),
-                            headers={'X-CSRFToken': csrftoken},
+                            headers={'X-CSRFToken': config.Token},
                             data=payload)
 
     if response.status_code == 200:
