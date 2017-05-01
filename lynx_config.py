f303ccd3b2fbcdb69a2195518234c36166debcde
Craig Derington <craig@craigderington.me>
2017-05-01 16:55:54 -0400

    Add process for Lynx Propane Software.

---
+++ lynx_config.py
@@ -0,0 +1,17 @@
+#! config.py
+
+DEBUG = True
+host = 'localhost'
+username = 'root'
+password = 'deadbeef'
+database = 'lanternv2'
+db_port = 3306
+PORT = 5555
+url = 'https://portal.owlsite.net/api/'
+tdh = 'tankdata/'
+tank = 'tanks/'
+loginusername = 'craigderington1'
+loginpassword = 'yufakay3'
+Token = '7BY4ZQHQTXRuL4FrmuVQGb1GqtBi6EIR'
+tank_id = 1315
+file_path = 'data/frontend_tankdatahistory_7026.csv'
