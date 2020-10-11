855ecd9596833f52045b99e421fa670d6f29b0aa
Craig Derington <craigderington17@gmail.com>
2020-10-11 09:53:34 -0400

    Update lynx_config.py
    Update for change to public visibility.

--- lynx_config.py
+++ lynx_config.py
@@ -2,16 +2,16 @@
 
 DEBUG = True
 host = 'localhost'
-username = 'root'
-password = 'deadbeef'
-database = 'lanternv2'
+username = ''
+password = ''
+database = ''
 db_port = 3306
 PORT = 5555
 url = 'https://portal.owlsite.net/api/'
 tdh = 'tankdata/'
 tank = 'tanks/'
-loginusername = 'craigderington1'
-loginpassword = 'yufakay3'
-Token = '7BY4ZQHQTXRuL4FrmuVQGb1GqtBi6EIR'
+loginusername = ''
+loginpassword = ''
+Token = ''
 tank_id = 1315
 file_path = 'data/frontend_tankdatahistory_7026.csv'
